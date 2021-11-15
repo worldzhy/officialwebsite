049a40c525e2315314fe4d3b7eec3bc2f0414cf2
Wanfu <nick@inceptionpad.com>
2021-11-15 19:56:30 +0800

    fix: fix overflow

--- src/pages/Home/components/Carousel.tsx
+++ src/pages/Home/components/Carousel.tsx
@@ -51,13 +51,14 @@ const StyledNavigator = styled.div`
 const StyledCarouselWrapper = styled(motion.div)`
   position: absolute;
   top: 0;
-  width: 100vw;
-  height: 100vh;
+  width: 100%;
+  height: 100%;
   background-color: black;
   z-index: 10;
   display: flex;
   align-items: center;
   justify-content: center;
+  overflow: hidden;
   .carousel-items-wrapper {
     display: flex;
     align-items: center;
