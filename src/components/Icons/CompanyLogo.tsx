e7e7c145641def45992184421c73b1703dd12584
Henry Zhao <henry@inceptionpad.com>
2021-12-14 17:14:57 +0800

    update logo svg

--- src/components/Icons/CompanyLogo.tsx
+++ src/components/Icons/CompanyLogo.tsx
@@ -11,8 +11,8 @@ const CompanyLogo: React.FC<SVGProps<any>> = (props) => {
       {...props}
     >
       <path 
-        fill-rule="evenodd" 
-        clip-rule="evenodd" 
+        fillRule="evenodd" 
+        clipRule="evenodd" 
         d="M40.3622 50.3016L59.3233 46.4L57.1233 41.6467L80 48.3714L39.7605 56.2571L0 48.3714L23.2951 41.4412L40.1616 5L56.9969 41.3737L40.5628 36.5428L23.6322 41.5796L21.4011 46.4L40.3622 50.3016ZM24.431 56.2572L39.7604 74.0001V60.2001L24.431 56.2572ZM55.0898 56.2572L39.7604 74.0001V60.2001L55.0898 56.2572Z" 
         fill="white"
       />
