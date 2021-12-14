d33998411819da3d77bfba450f7cbd947895d31c
Henry Zhao <henry@inceptionpad.com>
2021-12-14 18:04:25 +0800

    fix lint

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -69,7 +69,7 @@ const Header = () => {
   };
   return (
     <StyledHeader
-      className={"flex text-white space-between w-full text-lg"}
+      className={"flex text-white space-between w-full text-lg font-bold"}
     >
       <div
         className={"flex flex-1 aligns-center header-left"}
